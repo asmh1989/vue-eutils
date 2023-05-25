26e6eac19b4526316c4dd786397d446c98d10b80
sun <minhua.sun@aixplorerbio.com>
2023-05-25 16:19:30 +0800

    update

--- src/common/mode.ts
+++ src/common/mode.ts
@@ -33,25 +33,27 @@ export function getDoiUrl(doi: string) {
 
 export function copyToClipboard(textToCopy: string) {
   // navigator clipboard 需要https等安全上下文
-  //   if (navigator.clipboard && window.isSecureContext) {
-  //     // navigator clipboard 向剪贴板写文本
-  //     return navigator.clipboard.writeText(textToCopy);
-  //   } else {
-  // 创建text area
-  let textArea = document.createElement("textarea");
-  textArea.value = textToCopy;
-  // 使text area不在viewport，同时设置不可见
-  textArea.style.position = "absolute";
-  textArea.style.opacity = "0";
-  textArea.style.left = "-999999px";
-  textArea.style.top = "-999999px";
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-  return new Promise<void>((res, rej) => {
-    // 执行复制命令并移除文本框
-    document.execCommand("copy") ? res() : rej();
-    textArea.remove();
-  });
-  //   }
+  let force = false;
+  if (navigator.clipboard && window.isSecureContext && !force) {
+    // navigator clipboard 向剪贴板写文本
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    // 创建text area
+    console.log("use docment copy...");
+    let textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    // 使text area不在viewport，同时设置不可见
+    textArea.style.position = "absolute";
+    textArea.style.opacity = "0";
+    textArea.style.left = "-9999px";
+    textArea.style.top = "600px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise<void>((res, rej) => {
+      // 执行复制命令并移除文本框
+      document.execCommand("copy") ? res() : rej();
+      textArea.remove();
+    });
+  }
 }
